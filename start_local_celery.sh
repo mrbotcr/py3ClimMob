ade7a6b88fad5ab26492c0ce2934ab3b6ffb675c
Carlos Quiros <cquiros@qlands.com>
2020-08-14 22:10:11 -0600

    Use Redis

--- start_local_celery.sh
+++ start_local_celery.sh
@@ -1,1 +1,1 @@
-celery -A climmob.config.celery_app worker --loglevel=info --broker=redis://localhost:6379/0 --result-backend=redis://localhost:6379/0
+celery -A climmob.config.celery_app worker --loglevel=info --broker=redis://localhost:6379/5 --result-backend=redis://localhost:6379/5
