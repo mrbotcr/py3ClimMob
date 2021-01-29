3588993046a280f0d0eeb33af096da3435c3369a
qlands <cquiros@qlands.com>
2021-01-29 17:53:04 -0600

    Utility to push files like ODK Collect

---
+++ utilities/push_xml_example.py
@@ -0,0 +1,15 @@
+import requests
+import glob
+from requests.auth import HTTPDigestAuth
+
+headers = {"Content-Type": "text/xml"}
+for xml_file in glob.iglob(
+    "/path/to/xml/files/*.xml"
+):
+    xml_file = {"file": open(xml_file, "rb")}
+    r = requests.post(
+        "https://climmob.net/[climmob_user]/push",
+        auth=HTTPDigestAuth("[data_collector]", "[password]"),
+        files=xml_file,
+    )
+    print("File pushed {} with status {}".format(xml_file, r.status_code))
