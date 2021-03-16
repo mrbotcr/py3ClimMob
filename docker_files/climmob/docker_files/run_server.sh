ce12e374005f8da3f57c58d308efefeb9e06a2e3
qlands <cquiros@qlands.com>
2021-03-16 12:39:13 -0600

    Fix run with clean database

--- docker_files/climmob/docker_files/run_server.sh
+++ docker_files/climmob/docker_files/run_server.sh
@@ -6,9 +6,9 @@ source /opt/climmob_env/bin/activate
 cd /opt/climmob
 # If development.ini does not exists then create the basic database
 initial_database="false"
-if ! [ -f "/opt/climmob_config/development.ini" ]; then
+if [ ! -f /opt/climmob_config/development.ini ]; then
   initial_database="true"
-  mysql -h $MYSQL_HOST_NAME -u $MYSQL_USER_NAME --password=$MYSQL_USER_PASSWORD climmobv4 < /opt/climmob/docker_files/climmob/docker_files/base_db.sql
+  mysql -h $MYSQL_HOST_NAME -u $MYSQL_USER_NAME --ssl-mode=DISABLED --password=$MYSQL_USER_PASSWORD climmobv4 < /opt/climmob/docker_files/climmob/docker_files/base_db.sql
 fi
 
 python create_config.py --daemon --capture_output --mysql_host $MYSQL_HOST_NAME --mysql_user_name $MYSQL_USER_NAME --mysql_user_password $MYSQL_USER_PASSWORD --repository_path /opt/climmob_repository --odktools_path /opt/odktools --climmob_host $climmob_HOST --climmob_port $climmob_PORT --forwarded_allow_ip $FORWARDED_ALLOW_IP --pid_file /opt/climmob_gunicorn/climmob.pid --error_log_file /opt/climmob_log/error_log /opt/climmob_config/development.ini
