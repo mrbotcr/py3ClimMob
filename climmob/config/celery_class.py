364d8228b2edd9e36e86664b703d52d5695c8585
Brandon Madriz <b.madriz@cgiar.org>
2021-08-02 10:43:20 -0600

    Too many connections - Solution

--- climmob/config/celery_class.py
+++ climmob/config/celery_class.py
@@ -1,21 +1,23 @@
 from celery import Task
 from sqlalchemy import create_engine
 from climmob.config.celery_app import get_ini_value
-
+from sqlalchemy.pool import NullPool
 from celery.contrib.abortable import AbortableTask
 
 class celeryTask(AbortableTask):
     def on_success(self, retval, task_id, args, kwargs):
-        engine = create_engine(get_ini_value("sqlalchemy.url"))
-        engine.execute(
+        engine = create_engine(get_ini_value("sqlalchemy.url"), poolclass=NullPool)
+        connection = engine.connect()
+        connection.execute(
             "INSERT INTO finishedtasks(taskid,taskerror) VALUES ('"
             + str(task_id)
             + "',0)"
         )
+        connection.invalidate()
         engine.dispose()
 
     def on_failure(self, exc, task_id, args, kwargs, einfo):
-        engine = create_engine(get_ini_value("sqlalchemy.url"))
+        engine = create_engine(get_ini_value("sqlalchemy.url"), poolclass=NullPool)
         engine.execute(
             "INSERT INTO finishedtasks(taskid,taskerror) VALUES ('"
             + str(task_id)
--- climmob/models/repository.py
+++ climmob/models/repository.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from climmob.config.celery_app import get_ini_value
-
+from sqlalchemy.pool import NullPool
 
 def sql_fetch_one(sql):
-    engine = create_engine(get_ini_value("sqlalchemy.url"))
+    engine = create_engine(get_ini_value("sqlalchemy.url"), poolclass=NullPool)
     connection = engine.connect()
     res = connection.execute(sql).fetchone()
     connection.invalidate()
@@ -12,7 +12,7 @@ def sql_fetch_one(sql):
 
 
 def sql_fetch_all(sql):
-    engine = create_engine(get_ini_value("sqlalchemy.url"))
+    engine = create_engine(get_ini_value("sqlalchemy.url"), poolclass=NullPool)
     connection = engine.connect()
     res = connection.execute(sql).fetchall()
     connection.invalidate()
@@ -21,7 +21,7 @@ def sql_fetch_all(sql):
 
 
 def sql_execute(sql):
-    engine = create_engine(get_ini_value("sqlalchemy.url"))
+    engine = create_engine(get_ini_value("sqlalchemy.url"), poolclass=NullPool)
     connection = engine.connect()
     res = connection.execute(sql)
     connection.invalidate()
