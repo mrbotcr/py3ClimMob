3874e9552e8058d010f950404c41b236858e6d24
qlands <cquiros@qlands.com>
2022-10-11 08:19:10 -0400

    Add email to field agents

--- alembic/versions/48af04ae8c55_add_email_to_field_agents.py
+++ alembic/versions/48af04ae8c55_add_email_to_field_agents.py
@@ -10,15 +10,23 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import mysql
 
 # revision identifiers, used by Alembic.
-revision = '48af04ae8c55'
-down_revision = '6c0b8e792e2d'
+revision = "48af04ae8c55"
+down_revision = "6c0b8e792e2d"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.add_column('enumerator', sa.Column('enum_email', sa.Unicode(length=120), server_default=sa.text("''"), nullable=True))
+    op.add_column(
+        "enumerator",
+        sa.Column(
+            "enum_email",
+            sa.Unicode(length=120),
+            server_default=sa.text("''"),
+            nullable=True,
+        ),
+    )
 
 
 def downgrade():
-    op.drop_column('enumerator', 'enum_email')
+    op.drop_column("enumerator", "enum_email")
