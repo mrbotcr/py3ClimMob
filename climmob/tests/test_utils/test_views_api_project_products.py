2909d96c7556b533b6c6458fe8f62cfcc94c479a
Gonzalo Vargas <gvargas@mrbotcr.com>
2024-10-01 14:17:12 -0600

    Adding testing classes for test_views_api_project_products.py file n°2
    Executing black . and optimize imports

--- climmob/tests/test_utils/test_views_api_project_products.py
+++ climmob/tests/test_utils/test_views_api_project_products.py
@@ -1,13 +1,16 @@
 import json
 import unittest
-from unittest.mock import patch, MagicMock
-from pyramid.response import Response
 from datetime import datetime
+from unittest.mock import patch
+
+from pyramid.response import Response
+
+from climmob.tests.test_utils.common import BaseViewTestCase
 from climmob.views.Api.projectProducts import (
     readProducts_view,
     downloadApi_view
 )
-from climmob.tests.test_utils.common import BaseViewTestCase
+
 
 class TestReadProductsView(BaseViewTestCase):
     view_class = readProducts_view
@@ -15,20 +18,24 @@ class TestReadProductsView(BaseViewTestCase):
 
     def setUp(self):
         super().setUp()
-        self.request_body = json.dumps({
-            "project_cod": "PRJ123",
-            "user_owner": "owner_user"
-        })
+        self.request_body = json.dumps(
+            {"project_cod": "PRJ123", "user_owner": "owner_user"}
+        )
         self.view.body = self.request_body
         self.view.request.json_body = json.loads(self.request_body)
 
     @patch("climmob.views.Api.projectProducts.getTheProjectIdForOwner", return_value=1)
     @patch("climmob.views.Api.projectProducts.projectExists", return_value=True)
-    @patch("climmob.views.Api.projectProducts.getDataProduct", return_value=[
-        {"product_name": "Product 1", "created_at": datetime.now()},
-        {"product_name": "Product 2", "created_at": datetime.now()},
-    ])
-    def test_process_view_successful_retrieval(self, mock_get_products, mock_project_exists, mock_get_project_id):
+    @patch(
+        "climmob.views.Api.projectProducts.getDataProduct",
+        return_value=[
+            {"product_name": "Product 1", "created_at": datetime.now()},
+            {"product_name": "Product 2", "created_at": datetime.now()},
+        ],
+    )
+    def test_process_view_successful_retrieval(
+        self, mock_get_products, mock_project_exists, mock_get_project_id
+    ):
         response = self.view.processView()
         self.assertEqual(response.status_code, 200)
         products = json.loads(response.body.decode())
@@ -37,20 +44,21 @@ class TestReadProductsView(BaseViewTestCase):
         self.assertIn("Product 2", products[1]["product_name"])
         mock_project_exists.assert_called_once()
         mock_get_products.assert_called_once_with(1, self.view.request)
-        mock_get_project_id.assert_called_with('owner_user', 'PRJ123', self.view.request)
+        mock_get_project_id.assert_called_with(
+            "owner_user", "PRJ123", self.view.request
+        )
 
     @patch("climmob.views.Api.projectProducts.projectExists", return_value=False)
     def test_process_view_project_does_not_exist(self, mock_project_exists):
         response = self.view.processView()
         self.assertEqual(response.status_code, 401)
         self.assertIn("There is not project with that code.", response.body.decode())
-        mock_project_exists.assert_called_once_with('test_user', 'owner_user', 'PRJ123', self.view.request)
+        mock_project_exists.assert_called_once_with(
+            "test_user", "owner_user", "PRJ123", self.view.request
+        )
 
     def test_process_view_missing_parameters(self):
-        self.view.body = json.dumps({
-            "project_cod": "",
-            "user_owner": ""
-        })
+        self.view.body = json.dumps({"project_cod": "", "user_owner": ""})
         response = self.view.processView()
         self.assertEqual(response.status_code, 401)
         self.assertIn("Not all parameters have data.", response.body.decode())
@@ -72,8 +80,12 @@ class TestReadProductsView(BaseViewTestCase):
         self.view.body = ""
         response = self.view.processView()
         self.assertEqual(response.status_code, 401)
-        self.assertIn("Error in the JSON, It does not have the 'body' parameter.", response.body.decode())
+        self.assertIn(
+            "Error in the JSON, It does not have the 'body' parameter.",
+            response.body.decode(),
+        )
         mock_json_loads.assert_called_once()
+
 
 class TestDownloadApiView(BaseViewTestCase):
     view_class = downloadApi_view
@@ -81,42 +93,66 @@ class TestDownloadApiView(BaseViewTestCase):
 
     def setUp(self):
         super().setUp()
-        self.request_body = json.dumps({
-            "project_cod": "PRJ123",
-            "user_owner": "owner_user",
-            "celery_taskid": "task123",
-            "product_id": "prod123"
-        })
+        self.request_body = json.dumps(
+            {
+                "project_cod": "PRJ123",
+                "user_owner": "owner_user",
+                "celery_taskid": "task123",
+                "product_id": "prod123",
+            }
+        )
         self.view.body = self.request_body
         self.view.request.json_body = json.loads(self.request_body)
 
-
-    @patch("climmob.views.Api.projectProducts.FileResponse", return_value=Response(status=200))
-    @patch("climmob.views.Api.projectProducts.getProductDirectory", return_value="/fake/path/to/product")
+    @patch(
+        "climmob.views.Api.projectProducts.FileResponse",
+        return_value=Response(status=200),
+    )
+    @patch(
+        "climmob.views.Api.projectProducts.getProductDirectory",
+        return_value="/fake/path/to/product",
+    )
     @patch("climmob.views.Api.projectProducts.product_found", return_value=True)
-    @patch("climmob.views.Api.projectProducts.getProductData", return_value={
-        "product_id": "prod123",
-        "output_mimetype": "application/zip",
-        "output_id": "file123.zip"
-    })
+    @patch(
+        "climmob.views.Api.projectProducts.getProductData",
+        return_value={
+            "product_id": "prod123",
+            "output_mimetype": "application/zip",
+            "output_id": "file123.zip",
+        },
+    )
     @patch("climmob.views.Api.projectProducts.getTheProjectIdForOwner", return_value=1)
     @patch("climmob.views.Api.projectProducts.projectExists", return_value=True)
     def test_process_view_successful_download(
-            self, mock_project_exists, mock_get_project_id, mock_get_product_data,
-            mock_product_found, mock_get_product_directory, mock_file_response
+        self,
+        mock_project_exists,
+        mock_get_project_id,
+        mock_get_product_data,
+        mock_product_found,
+        mock_get_product_directory,
+        mock_file_response,
     ):
         response = self.view.processView()
 
         self.assertEqual(response.status_code, 200)
-        mock_project_exists.assert_called_once_with('test_user', 'owner_user', 'PRJ123', self.view.request)
-        mock_get_project_id.assert_called_once_with('owner_user', 'PRJ123', self.view.request)
-        mock_get_product_data.assert_called_once_with(1, 'task123', 'prod123', self.view.request)
-        mock_product_found.assert_called_once_with('prod123')
-        mock_get_product_directory.assert_called_once_with(self.view.request, 'owner_user', 'PRJ123', 'prod123')
-        mock_file_response.assert_called_once_with('/fake/path/to/product/outputs/file123.zip',
-                                                   request=self.view.request, content_type='application/zip')
-
-
+        mock_project_exists.assert_called_once_with(
+            "test_user", "owner_user", "PRJ123", self.view.request
+        )
+        mock_get_project_id.assert_called_once_with(
+            "owner_user", "PRJ123", self.view.request
+        )
+        mock_get_product_data.assert_called_once_with(
+            1, "task123", "prod123", self.view.request
+        )
+        mock_product_found.assert_called_once_with("prod123")
+        mock_get_product_directory.assert_called_once_with(
+            self.view.request, "owner_user", "PRJ123", "prod123"
+        )
+        mock_file_response.assert_called_once_with(
+            "/fake/path/to/product/outputs/file123.zip",
+            request=self.view.request,
+            content_type="application/zip",
+        )
 
     @patch("climmob.views.Api.projectProducts.projectExists", return_value=False)
     def test_process_view_project_does_not_exist(self, mock_project_exists):
@@ -124,46 +160,77 @@ class TestDownloadApiView(BaseViewTestCase):
 
         self.assertEqual(response.status_code, 401)
         self.assertIn("There is no project with that code.", response.body.decode())
-        mock_project_exists.assert_called_once_with('test_user', 'owner_user', 'PRJ123', self.view.request)
+        mock_project_exists.assert_called_once_with(
+            "test_user", "owner_user", "PRJ123", self.view.request
+        )
 
     @patch("climmob.views.Api.projectProducts.getTheProjectIdForOwner", return_value=1)
     @patch("climmob.views.Api.projectProducts.projectExists", return_value=True)
     @patch("climmob.views.Api.projectProducts.getProductData", return_value=None)
-    def test_process_view_product_not_found(self, mock_get_product_data, mock_project_exists, mock_get_project_id):
+    def test_process_view_product_not_found(
+        self, mock_get_product_data, mock_project_exists, mock_get_project_id
+    ):
         response = self.view.processView()
 
         self.assertEqual(response.status_code, 401)
-        self.assertIn("There is no product with that celery_taskid or product_id.", response.body.decode())
-        mock_project_exists.assert_called_once_with('test_user', 'owner_user', 'PRJ123', self.view.request)
-        mock_get_project_id.assert_called_once_with('owner_user', 'PRJ123', self.view.request)
-        mock_get_product_data.assert_called_once_with(1, 'task123', 'prod123', self.view.request)
-
+        self.assertIn(
+            "There is no product with that celery_taskid or product_id.",
+            response.body.decode(),
+        )
+        mock_project_exists.assert_called_once_with(
+            "test_user", "owner_user", "PRJ123", self.view.request
+        )
+        mock_get_project_id.assert_called_once_with(
+            "owner_user", "PRJ123", self.view.request
+        )
+        mock_get_product_data.assert_called_once_with(
+            1, "task123", "prod123", self.view.request
+        )
 
     @patch("climmob.views.Api.projectProducts.getTheProjectIdForOwner", return_value=1)
     @patch("climmob.views.Api.projectProducts.projectExists", return_value=True)
-    @patch("climmob.views.Api.projectProducts.getProductData", return_value={
-        "product_id": "prod123",
-        "output_mimetype": "application/zip",
-        "output_id": "file123.zip"
-    })
+    @patch(
+        "climmob.views.Api.projectProducts.getProductData",
+        return_value={
+            "product_id": "prod123",
+            "output_mimetype": "application/zip",
+            "output_id": "file123.zip",
+        },
+    )
     @patch("climmob.views.Api.projectProducts.product_found", return_value=False)
-    def test_process_view_invalid_product_id(self, mock_product_found, mock_get_product_data, mock_project_exists, mock_get_project_id):
+    def test_process_view_invalid_product_id(
+        self,
+        mock_product_found,
+        mock_get_product_data,
+        mock_project_exists,
+        mock_get_project_id,
+    ):
         response = self.view.processView()
 
         self.assertEqual(response.status_code, 401)
-        self.assertIn("There is no product with that product_id.", response.body.decode())
-        mock_project_exists.assert_called_once_with('test_user', 'owner_user', 'PRJ123', self.view.request)
-        mock_get_project_id.assert_called_once_with('owner_user', 'PRJ123', self.view.request)
-        mock_get_product_data.assert_called_once_with(1, 'task123', 'prod123', self.view.request)
-        mock_product_found.assert_called_once_with('prod123')
+        self.assertIn(
+            "There is no product with that product_id.", response.body.decode()
+        )
+        mock_project_exists.assert_called_once_with(
+            "test_user", "owner_user", "PRJ123", self.view.request
+        )
+        mock_get_project_id.assert_called_once_with(
+            "owner_user", "PRJ123", self.view.request
+        )
+        mock_get_product_data.assert_called_once_with(
+            1, "task123", "prod123", self.view.request
+        )
+        mock_product_found.assert_called_once_with("prod123")
 
     def test_process_view_missing_parameters(self):
-        self.view.body = json.dumps({
-            "project_cod": "",
-            "user_owner": "",
-            "celery_taskid": "task123",
-            "product_id": ""
-        })
+        self.view.body = json.dumps(
+            {
+                "project_cod": "",
+                "user_owner": "",
+                "celery_taskid": "task123",
+                "product_id": "",
+            }
+        )
         response = self.view.processView()
 
         self.assertEqual(response.status_code, 401)
@@ -189,7 +256,10 @@ class TestDownloadApiView(BaseViewTestCase):
         response = self.view.processView()
 
         self.assertEqual(response.status_code, 401)
-        self.assertIn("Error in the JSON, It does not have the 'body' parameter.", response.body.decode())
+        self.assertIn(
+            "Error in the JSON, It does not have the 'body' parameter.",
+            response.body.decode(),
+        )
         mock_json_loads.assert_called_once()
 
 
