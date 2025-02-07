7c29b70e17aa3270a19cf604855c836f029987bc
bmadriz <bmadriz@mrbotcr.com>
2025-02-07 13:50:59 -0600

    Change middleware

--- climmob/middleware/middleware.py
+++ climmob/middleware/middleware.py
@@ -35,43 +35,44 @@ def CheckMiddleware(event):
             return
 
         last_activity = getLastActivityLogByUser(login, request)
-        if last_activity["log_message"] != "Welcome to ClimMob":
+        if last_activity:
+            if last_activity["log_message"] != "Welcome to ClimMob":
 
-            list_of_path = []
+                list_of_path = []
 
-            list_of_path.append("otherLanguages")
-            list_of_path.append("getUserLanguagesPreview")
-            list_of_path.append("addUserLanguage")
-            list_of_path.append("APIlanguages")
+                list_of_path.append("otherLanguages")
+                list_of_path.append("getUserLanguagesPreview")
+                list_of_path.append("addUserLanguage")
+                list_of_path.append("APIlanguages")
 
-            if request.matched_route.name not in list_of_path:
-                if not getListOfLanguagesByUser(request, login):
-                    raise HTTPFound(
-                        location=request.route_url(
-                            "otherLanguages", _query={"help": "languages"}
+                if request.matched_route.name not in list_of_path:
+                    if not getListOfLanguagesByUser(request, login):
+                        raise HTTPFound(
+                            location=request.route_url(
+                                "otherLanguages", _query={"help": "languages"}
+                            )
                         )
+
+                """list_of_path.append("curationoftechnologies")
+                list_of_path.append("APICrops")
+    
+                if request.matched_route.name not in list_of_path:
+                    completed, results = getTechnologiesByUserWithoutCropTaxonomy(
+                        login, request
                     )
-
-            """list_of_path.append("curationoftechnologies")
-            list_of_path.append("APICrops")
-
-            if request.matched_route.name not in list_of_path:
-                completed, results = getTechnologiesByUserWithoutCropTaxonomy(
-                    login, request
-                )
-                if not completed:
-                    raise HTTPFound(
-                        location=request.route_url("curationoftechnologies")
-                    )
-
-            list_of_path.append("curationofprojects")
-            list_of_path.append("getunitofanalysisbylocation")
-            list_of_path.append("getobjectivesbylocationandunitofanalysis")
-            list_of_path.append("searchaffiliation")
-
-            if request.matched_route.name not in list_of_path:
-
-                completed, projects = getProjectsByUserThatRequireSetup(login, request)
-
-                if not completed:
-                    raise HTTPFound(location=request.route_url("curationofprojects"))"""
+                    if not completed:
+                        raise HTTPFound(
+                            location=request.route_url("curationoftechnologies")
+                        )
+    
+                list_of_path.append("curationofprojects")
+                list_of_path.append("getunitofanalysisbylocation")
+                list_of_path.append("getobjectivesbylocationandunitofanalysis")
+                list_of_path.append("searchaffiliation")
+    
+                if request.matched_route.name not in list_of_path:
+    
+                    completed, projects = getProjectsByUserThatRequireSetup(login, request)
+    
+                    if not completed:
+                        raise HTTPFound(location=request.route_url("curationofprojects"))"""
