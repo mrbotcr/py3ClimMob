d1d8435fc3c28b324d82bfd20b1d64e77ed28277
JohannMrBot <javalos@mrbotcr.com>
2025-04-01 14:47:08 -0600

    onboard: add views for project_objective

---
+++ climmob/views/prj_objective.py
@@ -0,0 +1,70 @@
+from pyramid.response import Response
+
+from climmob.models import ProjectObjectives
+from climmob.processes.db.project_objectives import get_objective_by_id
+from climmob.views.classes import privateView
+
+from climmob.processes import (
+    get_all_project_location,
+    add_objective,
+    update_objective,
+    delete_objective_by_id,
+)
+
+
+class objective_by_id_view(privateView):
+    def processView(self):
+        print(
+            f"{self.request.method} objective by id {self.request.matchdict['objective_id']}"
+        )
+        pobj_id = self.request.matchdict["objective_id"]
+        if self.request.method == "GET":
+            self.returnRawViewResult = True
+            return get_objective_by_id(self.request, pobj_id)
+
+        elif self.request.method == "PATCH":
+            self.returnRawViewResult = True
+            new_name = self.request.json_body["pobjective_name"]
+            success, msg = update_objective(
+                self.request,
+                ProjectObjectives(pobjective_id=pobj_id, pobjective_name=new_name),
+            )
+            if not success:
+                return Response(msg, status="400")
+            return get_objective_by_id(self.request, pobj_id)
+
+        elif self.request.method == "DELETE":
+            delete_objective_by_id(self.request, pobj_id)
+            self.returnRawViewResult = True
+            return {"status": 200}
+
+
+class prj_objectives_view(privateView):
+    def processView(self):
+        dataworking = {"project_location": "-1", "project_unit_of_analysis": "-1"}
+        error_summary = {}
+        modify = False
+        reportUpload = []
+
+        nextPage = self.request.params.get("next")
+
+        print(f"{self.request.method} project objectives")
+
+        if self.request.method == "POST":
+            # dataworking = {...dataworking, self.getPostDict()}
+            body = self.getPostDict()
+            success, msg = add_objective(self.request, body.get("pobjective_name"))
+            if not success:
+                error_summary = {"error": self._(msg)}
+            print(body)
+
+        return {
+            "activeUser": self.user,
+            "dataworking": dataworking,
+            "error_summary": error_summary,
+            "reportUpload": reportUpload,
+            "modify": modify,
+            "nextPage": nextPage,
+            "sectionActive": "prj_objectives",
+            "listOfLocations": get_all_project_location(self.request),
+        }
