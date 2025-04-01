7a687a4e96280dddf44ed721671e4e1718f313bf
JohannMrBot <javalos@mrbotcr.com>
2025-04-01 14:42:23 -0600

    onboard: add CRUD methods to project_objective db processes

--- climmob/processes/db/project_objectives.py
+++ climmob/processes/db/project_objectives.py
@@ -1,3 +1,6 @@
+from pyramid.httpexceptions import HTTPNotFound
+from sqlalchemy.exc import NoResultFound
+
 from climmob.models import (
     ProjectObjectives,
     I18nProjectObjectives,
@@ -9,13 +12,91 @@ from sqlalchemy import func, and_
 
 __all__ = [
     "get_all_objectives_by_location_and_unit_of_analysis",
+    "add_objective",
+    "get_objective_by_id",
+    "update_objective",
+    "delete_objective_by_id",
 ]
 
+from climmob.processes import getAllLocationUnitOfAnalysis
+
+def get_objective_by_name(request, name):
+    try:
+        return (
+            request.dbsession.query(ProjectObjectives)
+            .filter(ProjectObjectives.pobjective_name == name)
+            .one()
+        )
+    except NoResultFound:
+        raise HTTPNotFound
+
+def get_objective_by_id(request, obj_id):
+    try:
+        res = (
+            request.dbsession.query(ProjectObjectives)
+            .filter(ProjectObjectives.pobjective_id == obj_id)
+            .one()
+        )
+        return mapFromSchema(res)
+    except NoResultFound:
+        raise HTTPNotFound
+
+def add_objective(request, name):
+    try:
+        get_objective_by_name(request, name)
+        return False, "Objective already exists"
+    except HTTPNotFound:
+        pass
+    new_objective = ProjectObjectives()
+    new_objective.pobjective_name = name
+    new_objective.pobjective_lang = "en"
+    try:
+        request.dbsession.add(new_objective)
+        new_objective = get_objective_by_name(request, name)
+
+        uoas = getAllLocationUnitOfAnalysis(request)
+        for uoa in uoas:
+
+            new_luoa = LocationUnitOfAnalysisObjectives()
+            new_luoa.pobjective_id = new_objective.pobjective_id
+            new_luoa.pluoa_id = uoa["pluoa_id"]
+
+            request.dbsession.add(new_luoa)
+        return True, ""
+    except Exception as e:
+        return False, str(e)
+
+
+def delete_objective_by_id(request, obj_id):
+    try:
+        (
+            request.dbsession.query(ProjectObjectives)
+            .filter(ProjectObjectives.pobjective_id == obj_id)
+            .delete()
+        )
+        return True, ""
+    except Exception as e:
+        return False, e
+
+def update_objective(request, objective: ProjectObjectives):
+    try:
+        get_objective_by_name(request, objective.pobjective_name)
+        return False, "Objective name is already in use."
+    except HTTPNotFound:
+        pass
+    try:
+        (
+            request.dbsession.query(ProjectObjectives)
+            .filter(ProjectObjectives.pobjective_id == objective.pobjective_id)
+            .update({"pobjective_name": objective.pobjective_name})
+        )
+        return True, ""
+    except Exception as e:
+        return False, e
 
 def get_all_objectives_by_location_and_unit_of_analysis(
     request, location_id, unit_of_analysis_id
 ):
-
     sub_query_LocationUnitOfAnalysis = (
         request.dbsession.query(LocationUnitOfAnalysis.pluoa_id)
         .filter(LocationUnitOfAnalysis.plocation_id == location_id)
