3511e95686dece96d0a99d65846f129448f88116
JohannMrBot <javalos@mrbotcr.com>
2025-04-02 19:18:12 -0600

    feat: add database methods for getting, creating and deleting location_unit_of_analysis_objective

--- climmob/processes/db/location_unit_of_analysis_objectives.py
+++ climmob/processes/db/location_unit_of_analysis_objectives.py
@@ -3,13 +3,17 @@ from climmob.models import (
     mapFromSchema,
 )
 
-__all__ = ["get_location_unit_of_analysis_objectives_by_combination"]
+__all__ = [
+    "get_location_unit_of_analysis_objectives_by_combination",
+    "get_location_unit_of_analysis_objectives_by_pobjective_id",
+    "add_location_unit_of_analysis_objective",
+    "delete_location_unit_of_analysis_objective",
+]
 
 
 def get_location_unit_of_analysis_objectives_by_combination(
     request, pluoa_id, pobjective_id
 ):
-
     result = mapFromSchema(
         request.dbsession.query(LocationUnitOfAnalysisObjectives)
         .filter(LocationUnitOfAnalysisObjectives.pluoa_id == pluoa_id)
@@ -18,3 +22,34 @@ def get_location_unit_of_analysis_objectives_by_combination(
     )
 
     return result
+
+
+def get_location_unit_of_analysis_objectives_by_pobjective_id(request, pobjective_id):
+    result = mapFromSchema(
+        request.dbsession.query(LocationUnitOfAnalysisObjectives)
+        .filter(LocationUnitOfAnalysisObjectives.pobjective_id == pobjective_id)
+        .all()
+    )
+    return result
+
+
+def add_location_unit_of_analysis_objective(request, pobjective_id, pluoa_id):
+    new_pluoaobj = LocationUnitOfAnalysisObjectives(
+        pobjective_id=pobjective_id, pluoa_id=pluoa_id
+    )
+    try:
+        request.dbsession.add(new_pluoaobj)
+        return True, None
+    except Exception as e:
+        return False, "Could not add new objective"
+
+
+def delete_location_unit_of_analysis_objective(request, pluoaobj_id):
+    try:
+        (
+            request.dbsession.query(LocationUnitOfAnalysisObjectives)
+            .filter(LocationUnitOfAnalysisObjectives.pluoaobj_id == pluoaobj_id)
+            .delete()
+        )
+    except Exception as e:
+        return False, "Could not delete location_unit_of_analysis_objective"
