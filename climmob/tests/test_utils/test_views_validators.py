7af8e71fc2bfc560ae811c5b33446f1dbfb637ac
JohannMrBot <javalos@mrbotcr.com>
2025-04-14 13:26:51 -0600

    #337: add tests for project exists validator

--- climmob/tests/test_utils/test_views_validators.py
+++ climmob/tests/test_utils/test_views_validators.py
@@ -1,7 +1,10 @@
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
+
+from pyramid.httpexceptions import HTTPNotFound
 
 from climmob.views.validators.BaseValidator import BaseValidator
+from climmob.views.validators.ProjectExistsValidator import ProjectExistsValidator
 
 
 class TestBaseValidator(unittest.TestCase):
@@ -13,3 +16,47 @@ class TestBaseValidator(unittest.TestCase):
     def test_run(self):
         with self.assertRaises(NotImplementedError):
             self.validator.run()
+
+
+class TestProjectExistsValidator(unittest.TestCase):
+    def setUp(self):
+        self.request = MagicMock()
+        self.request.matchdict = {"user": "test_user", "project": "test_project"}
+
+        self.view = MagicMock()
+        self.view.user = MagicMock()
+        self.view.user.login = "test_user_login"
+        self.view.request = self.request
+
+        self.validator = ProjectExistsValidator(self.view)
+
+    @patch(
+        "climmob.views.validators.ProjectExistsValidator.projectExists",
+        return_value=True,
+    )
+    def test_run_valid(self, mock_project_exists):
+        result = self.validator.run()
+
+        mock_project_exists.assert_called_once_with(
+            self.validator.view.user.login,
+            self.request.matchdict["user"],
+            self.request.matchdict["project"],
+            self.request,
+        )
+
+        self.assertEqual(result, None)
+
+    @patch(
+        "climmob.views.validators.ProjectExistsValidator.projectExists",
+        return_value=False,
+    )
+    def test_run_invalid(self, mock_project_exists):
+        with self.assertRaises(HTTPNotFound):
+            self.validator.run()
+
+        mock_project_exists.assert_called_once_with(
+            self.validator.view.user.login,
+            self.request.matchdict["user"],
+            self.request.matchdict["project"],
+            self.request,
+        )
