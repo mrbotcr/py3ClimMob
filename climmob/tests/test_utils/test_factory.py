96825a65beec0903aaf92dc2f48d6879206cfb7a
JohannMrBot <javalos@mrbotcr.com>
2025-04-22 17:13:42 -0600

    #339: add check for already existing attributes in the request

--- climmob/tests/test_utils/test_factory.py
+++ climmob/tests/test_utils/test_factory.py
@@ -8,6 +8,8 @@ class TestFactory(unittest.TestCase):
     def setUp(self):
         self.request = MagicMock()
         self.request.matchdict = {"user": "test_user", "project": "test_project"}
+        self.request.user = None
+        self.request.project = None
 
     def test_factory(self):
 
@@ -15,3 +17,17 @@ class TestFactory(unittest.TestCase):
 
         self.assertEqual(self.request.user, self.request.matchdict["user"])
         self.assertEqual(self.request.project, self.request.matchdict["project"])
+
+    def test_factory_attribute_already_used(self):
+        self.request.matchdict["method"] = "test_method"
+
+        self.request.method = "GET"
+
+        with self.assertRaises(AttributeError) as context:
+            factory(self.request)
+
+        self.assertEqual(self.request.method, "GET")
+
+        self.assertEqual(
+            str(context.exception), "request already has attribute 'method'"
+        )
--- climmob/utility/factory.py
+++ climmob/utility/factory.py
@@ -1,5 +1,7 @@
 # Add all parameters from matchdict to the request as attributes
 def factory(request):
     for key, value in request.matchdict.items():
+        attribute = getattr(request, key, None)
+        if attribute is not None:
+            raise AttributeError(f"request already has attribute '{key}'")
         setattr(request, key, value)
-    return None
