7a2ae0ecce4274cb1a9192f9816340a5f72ec106
JohannMrBot <javalos@mrbotcr.com>
2025-06-24 15:48:42 -0600

    add base test classes

--- climmob/tests/test_utils/common.py
+++ climmob/tests/test_utils/common.py
@@ -2,6 +2,32 @@ import unittest
 from unittest.mock import MagicMock, patch
 
 
+class BaseTest(unittest.TestCase):
+    patchers = {}
+    mocks = {}
+
+    def setUp(self):
+        for key in self.mocks:
+            self.mocks[key].reset_mock()
+            self.mocks[key].return_value = self.patchers[key]["return_value"]
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        super().setUpClass() must be called at the end of subclasses' setUpClass().
+        """
+        for key in cls.patchers:
+            cls.mocks[key] = cls.patchers[key]["patch"].start()
+
+    @classmethod
+    def tearDownClass(cls):
+        for key in cls.patchers:
+            cls.patchers[key]["patch"].stop()
+
+    def get_mock(self, name):
+        return self.mocks[name]
+
+
 class ViewBaseTest(unittest.TestCase):
     view_class = None
     request_method = "GET"
@@ -19,3 +45,14 @@ class ViewBaseTest(unittest.TestCase):
 
     def mock_translation(self, message):
         return message
+
+
+class PrivateViewBaseTest(BaseTest):
+    view_class = None
+
+    def setUp(self):
+        super().setUp()
+        self.request = MagicMock()
+        self.request.translate = lambda x: x
+        with patch("climmob.views.classes.PrivateContext"):
+            self.view = self.view_class(self.request)
