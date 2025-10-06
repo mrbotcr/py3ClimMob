560c7b8ba052e694adc9543fb5e4bc703e7ed8ff
JohannMrBot <javalos@mrbotcr.com>
2025-10-06 11:05:00 -0600

    remove unnecessary endpoint

--- climmob/config/routes.py
+++ climmob/config/routes.py
@@ -245,7 +245,6 @@ from climmob.views.registry import (
     GetRegistrySectionView,
     ChangeProjectMainLanguage_view,
 )
-from climmob.views.results import ResultsView
 from climmob.views.techaliases import deletealias_view
 from climmob.views.technologies import (
     technologies_view,
@@ -2163,15 +2162,6 @@ def loadRoutes(config):
             "/api/generateAnalysisByApi",
             GenerateAnalysisByApiViewApi,
             None,
-        )
-    )
-
-    routes.append(
-        addRoute(
-            "results",
-            "/api/results",
-            ResultsView,
-            "json",
         )
     )
 
