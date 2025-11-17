0c786f6a96a3cb6b9a1583242b1d29a44d1623e0
PabloMrBot <porozco@mrbotcr.com>
2025-11-17 11:13:35 -0600

    delete print

--- climmob/processes/db/project.py
+++ climmob/processes/db/project.py
@@ -35,7 +35,10 @@ from climmob.processes.db.project_location_unit_objective import (
 from climmob.processes.db.project_metadata_form import (
     knowIfTheProjectMetadataIsComplete,
 )
-from climmob.processes.db.project_technologies import numberOfCombinationsForTheProject, searchTechnologiesInProject
+from climmob.processes.db.project_technologies import (
+    numberOfCombinationsForTheProject,
+    searchTechnologiesInProject,
+)
 from climmob.processes.db.question import getQuestionOptions
 
 __all__ = [
@@ -505,7 +508,6 @@ def getUserProjects(user, request):
         .all()
     )
     mappedData = mapFromSchema(projects)
-    print(mappedData)
     for project in mappedData:
         project["owner"] = mapFromSchema(
             request.dbsession.query(userProject)
@@ -528,7 +530,9 @@ def getUserProjects(user, request):
             .filter(Country.cnty_cod == project["project_cnty"])
             .one()
         )["cnty_name"]
-        project["technologies"] = searchTechnologiesInProject(project["project_id"], request)
+        project["technologies"] = searchTechnologiesInProject(
+            project["project_id"], request
+        )
     return mappedData
 
 
