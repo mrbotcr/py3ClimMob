f65accc3f979d7d053d95e7fe48e76974ee45f97
bmadriz <bmadriz@mrbotcr.com>
2025-11-27 19:09:53 -0600

    Changes for release 3.9.2

--- setup.py
+++ setup.py
@@ -192,7 +192,7 @@ tests_require = [
 
 setup(
     name="climmob",
-    version="3.9.1",
+    version="3.9.2",
     description="Climmob",
     long_description=README + "\n\n" + CHANGES,
     classifiers=[
