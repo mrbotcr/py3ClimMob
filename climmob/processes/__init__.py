ace6941b6deb3b1d023a32b4f7aafd8fc88af1bc
JohannMrBot <javalos@mrbotcr.com>
2026-01-06 08:09:08 -0600

    add db process for saving publish status

--- climmob/processes/__init__.py
+++ climmob/processes/__init__.py
@@ -51,3 +51,4 @@ from climmob.processes.db.project_location_unit_objective import *
 from climmob.processes.db.location_unit_of_analysis_objectives import *
 from climmob.processes.db.affiliation import *
 from climmob.processes.db.project_summary import *
+from climmob.processes.db.project_publish_status import *
