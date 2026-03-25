2cd065c3790c0d3c648ea0d5d5c284fd8e39bf4b
JohannMrBot <javalos@mrbotcr.com>
2026-03-25 14:19:05 -0600

    fix validator condition

--- climmob/views/validators/project/IsProjectOwnerValidator.py
+++ climmob/views/validators/project/IsProjectOwnerValidator.py
@@ -8,7 +8,7 @@ class IsProjectOwnerValidator(BaseValidator):
     def run(self):
         access_type = self.view.context.access_type
 
-        if access_type == ProjectAccessType.OWNER.value:
+        if access_type != ProjectAccessType.OWNER.value:
             raise HTTPForbidden(
                 self._(
                     "The access assigned for this project does not allow you to publish data."
