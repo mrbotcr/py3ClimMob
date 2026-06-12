c45450556900e5de45c830acfe2639f44352ee23
JohannMrBot <javalos@mrbotcr.com>
2026-06-12 17:14:25 -0600

    change project publication status process for global status

--- climmob/processes/db/project_publication_status.py
+++ climmob/processes/db/project_publication_status.py
@@ -1,10 +1,14 @@
 import datetime
 
-__all__ = ["save_project_publication_status"]
+__all__ = [
+    "save_project_publication_status",
+    "get_global_project_publication_status_id",
+]
 
 from sqlalchemy import and_
 
 from climmob.models.climmobv4 import ProjectPublicationStatus
+from climmob.utility import PublicationStatus
 
 
 def save_project_publication_status(
@@ -28,3 +32,41 @@ def save_project_publication_status(
         request.dbsession.add(project_publication_status)
     else:
         res.update({"publication_status_id": status})
+
+
+def get_global_project_publication_status_id(request, project_id: str):
+    res = (
+        request.dbsession.query(ProjectPublicationStatus)
+        .filter(ProjectPublicationStatus.project_id == project_id)
+        .all()
+    )
+    if not res:
+        return PublicationStatus.INITIAL
+
+    if any(status.publication_status_id == PublicationStatus.FAILED for status in res):
+        return PublicationStatus.FAILED
+
+    if any(
+        status.publication_status_id == PublicationStatus.PUBLISHED for status in res
+    ):
+        return PublicationStatus.PUBLISHED
+
+    if any(
+        status.publication_status_id == PublicationStatus.APPROVED for status in res
+    ):
+        return PublicationStatus.APPROVED
+
+    if any(
+        status.publication_status_id == PublicationStatus.REJECTED for status in res
+    ):
+        return PublicationStatus.REJECTED
+
+    if any(
+        status.publication_status_id == PublicationStatus.IN_REVIEW for status in res
+    ):
+        return PublicationStatus.IN_REVIEW
+
+    if any(
+        status.publication_status_id == PublicationStatus.REQUESTED for status in res
+    ):
+        return PublicationStatus.REQUESTED
