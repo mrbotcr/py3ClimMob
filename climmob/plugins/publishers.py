ecfcac8bf3ffdab12bdf3be9a0819f0d0924b742
JohannMrBot <javalos@mrbotcr.com>
2026-07-02 10:48:59 -0600

    add climmob publisher derived class

---
+++ climmob/plugins/publishers.py
@@ -0,0 +1,17 @@
+from pyutilib.component.core import implements
+
+from climmob.plugins import SingletonPlugin, IPublisher
+
+
+class ClimMobPublisher(SingletonPlugin):
+    implements(IPublisher)
+    destination_name = "climmob"
+    label = "ClimMob"
+    disabled = True
+    index = 0
+
+    def get_destination_name(self):
+        return self.destination_name
+
+    def get_label(self):
+        return self.label
