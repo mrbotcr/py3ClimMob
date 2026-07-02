d6cb78a5833685ce8673bf7bc08d32739332a433
JohannMrBot <javalos@mrbotcr.com>
2026-07-02 10:12:10 -0600

    add process to get project license

--- climmob/processes/db/project_publication_license.py
+++ climmob/processes/db/project_publication_license.py
@@ -2,6 +2,7 @@ from climmob.models.climmobv4 import ProjectPublicationLicense
 
 __all__ = [
     "save_project_publication_license",
+    "get_project_publication_license_id",
 ]
 
 
@@ -19,3 +20,14 @@ def save_project_publication_license(project_id, license_id, request):
                 project_id=project_id, publication_license_id=license_id
             )
         )
+
+
+def get_project_publication_license_id(project_id, request):
+    license = (
+        request.dbsession.query(ProjectPublicationLicense.publication_license_id)
+        .filter_by(project_id=project_id)
+        .first()
+    )
+    if license:
+        return license[0]
+    return None
