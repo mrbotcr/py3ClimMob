b2bb5fa4a50a8e131470429bb17322ccc7df0ce5
JohannMrBot <javalos@mrbotcr.com>
2026-07-02 13:15:08 -0600

    feat: validate license in POST and emails

--- climmob/utility/email.py
+++ climmob/utility/email.py
@@ -4,6 +4,7 @@ from email import utils
 from email.header import Header
 from email.mime.text import MIMEText
 from time import time
+from typing import TypedDict
 
 from climmob.config.jinja_extensions import jinjaEnv
 
@@ -14,6 +15,8 @@ __all__ = [
     "build_email_message",
     "build_email_message_multiple_recipients",
     "EmailSender",
+    "build_email",
+    "EmailBuilder",
 ]
 
 
@@ -46,6 +49,78 @@ def build_email_message_multiple_recipients(body, subject, recipients, mail_from
     msg["Date"] = utils.formatdate(time())
 
     return msg
+
+
+def build_email(mail_from, recipients, subject, template, context):
+    try:
+        text = render_template(template, context)
+    except Exception as e:
+        log.error(f"Error rendering email template: {e}")
+        return
+
+    try:
+        msg = build_email_message_multiple_recipients(
+            text, subject, recipients, mail_from
+        )
+    except Exception as e:
+        log.error(f"Error building email message: {e}")
+        return
+    return msg
+
+
+class EmailRecipient(TypedDict):
+    user_fullname: str
+    user_email: str
+
+
+class EmailBuilder:
+    def __init__(self, settings):
+        self._mail_from = settings.get("email.from", None)
+        self._recipients: list[tuple] | None = None
+        self._subject: str | None = None
+        self._template: str | None = None
+        self._context: dict | None = None
+
+    def recipients(self, recipients: list[EmailRecipient]):
+        self._recipients = []
+        for user in recipients:
+            self._recipients.append((user["user_fullname"], user["user_email"]))
+        return self
+
+    def subject(self, subject):
+        self._subject = subject
+        return self
+
+    def template(self, template):
+        # TODO: Validate template existence
+        self._template = template
+        return self
+
+    def context(self, context):
+        self._context = context
+        return self
+
+    def build(self):
+        if not self._recipients:
+            log.warning("Email didn't send. No recipients found.")
+            return
+        if not self._subject:
+            log.warning("Email didn't send. No subject found.")
+            return
+        if not self._template:
+            log.warning("Email didn't send. No template found.")
+            return
+        if not self._context:
+            log.warning("Email didn't send. No context found.")
+            return
+        msg = build_email(
+            self._mail_from,
+            self._recipients,
+            self._subject,
+            self._template,
+            self._context,
+        )
+        return msg
 
 
 class EmailSender:
