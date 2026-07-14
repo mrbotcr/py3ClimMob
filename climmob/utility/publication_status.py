fa6a335c6eee7d8b2f16ef8218ec1d16f5a50f3c
JohannMrBot <javalos@mrbotcr.com>
2026-07-14 08:56:14 -0600

    fix approval column

--- climmob/utility/publication_status.py
+++ climmob/utility/publication_status.py
@@ -1,10 +1,14 @@
 from enum import Enum, IntEnum
 
 
+def _(x):
+    return x
+
+
 class PublicationStatus(IntEnum):
     INITIAL = 1
     REQUESTED = 2
-    IN_REVIEW = 3
+    # IN_REVIEW = 3 # TODO: update alembic revision
     APPROVED = 4
     REJECTED = 5
     PUBLISHED = 6
@@ -14,41 +18,45 @@ class PublicationStatus(IntEnum):
 class PublicationStatusLabel(Enum):
     INITIAL = "Initial"
     REQUESTED = "Requested"
-    IN_REVIEW = "In review"
+    # IN_REVIEW = "In review"
     APPROVED = "Approved"
     REJECTED = "Rejected"
     PUBLISHED = "Published"
     FAILED = "Failed"
 
 
-class PublicationStatusCanEdit(Enum):
-    INITIAL = False
-    REQUESTED = True
-    IN_REVIEW = True
+class PublicationStatusOption(Enum):
+    APPROVED = PublicationStatus.APPROVED
+    REJECTED = PublicationStatus.REJECTED
+
+
+class PublicationStatusOptionsEnabled(Enum):
     APPROVED = False
-    REJECTED = False
-    PUBLISHED = False
-    FAILED = False
+    REJECTED = True
 
 
-class PublicationStatusCanBeSelected(Enum):
-    INITIAL = False
-    REQUESTED = False
-    IN_REVIEW = True
+class PublicationStatusOptionSelectable(Enum):
     APPROVED = True
     REJECTED = True
-    PUBLISHED = False
-    FAILED = False
 
 
 def get_publication_status_options():
-    options = []
-    for status in PublicationStatus:
+    options = [
+        {"value": -1, "label": _("---"), "selectable": True, "editable": False},
+        {
+            "value": 0,
+            "label": _("Choose an option"),
+            "selectable": True,
+            "editable": True,
+        },
+    ]
+
+    for status in PublicationStatusOption:
         option = {
-            "value": status,
+            "value": status.value,
             "label": PublicationStatusLabel[status.name].value,
-            "selectable": PublicationStatusCanBeSelected[status.name].value,
-            "editable": PublicationStatusCanEdit[status.name].value,
+            "selectable": PublicationStatusOptionSelectable[status.name].value,
+            "editable": PublicationStatusOptionsEnabled[status.name].value,
         }
         options.append(option)
     return options
